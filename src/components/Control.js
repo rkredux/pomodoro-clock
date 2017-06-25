2e6efc809ccceb23d385b3248ffc7df07b4f28d5
Rahul <rkredux@gmail.com>
2017-06-25 01:27:31 -0400

    Add updation functionality in Control buttons

--- src/components/Control.js
+++ src/components/Control.js
@@ -6,9 +6,9 @@ class Control extends Component {
 	render(){
 		return(
 			<div className="control">
-			   <button onClick={this.props.stopTimer} className="stop">||</button>
-			   <button onClick={this.props.startTimer} className="start">Start</button>
-			   <button onClick={this.props.resetTimer} className="reset">Reset</button>
+			   <button onClick={this.props.stopTimer} className="stop">Hold On</button>
+			   <button onClick={this.props.startTimer} className="start">Let's Go</button>
+			   <button onClick={this.props.resetTimer} className="reset">Start Again</button>
 			</div>
 			); 
 	}
