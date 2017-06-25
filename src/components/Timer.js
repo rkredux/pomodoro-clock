09513c4b036bc515533b093c94fc6ffcb0c485e5
Rahul <rkredux@gmail.com>
2017-06-25 01:28:36 -0400

    Update timer component render function

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,12 +5,19 @@ class Timer extends Component{
 
 
 	render(){
-		const times = this.props.time
+
+		const times = {
+			hr: this.props.time.hrs, 
+			min: this.props.time.mins, 
+			sec: this.props.time.seconds
+		}
+
+
 		return(
 			<div className="timer">
 			   {Object
 			   	.keys(times)
-			   	.map((time) => <h2 key={time}>{times[time]}</h2>)
+			   	.map((time) => <h2 key={time}>{times[time]}{time}</h2>)
 			   }
 			</div>
 		); 
