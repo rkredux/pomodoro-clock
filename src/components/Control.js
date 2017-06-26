f75f0c51b91194df4686eb3b59ad85952db652e6
Rahul <rkredux@gmail.com>
2017-06-26 12:32:12 -0400

    Add correct class name

--- src/components/Control.js
+++ src/components/Control.js
@@ -8,7 +8,7 @@ class Control extends Component {
 			<div className="control">
 			   <button onClick={this.props.stopTimer} className="stop">Hold On</button>
 			   <button onClick={this.props.startTimer} className="start">Let's Go</button>
-			   <button onClick={this.props.resetTimer} className="reset">Start Again</button>
+			   <button onClick={this.props.resetTimer} className="reset">Reset</button>
 			</div>
 			); 
 	}
